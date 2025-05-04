d3b22d96b42e3c3bf486c1c4c5be91b209ec0eb1
Jabulani Ndlovu <jabulani.ndlovu360@gmail.com>
2025-05-04 11:09:22 +0200

    Created math import module
    Here I saw how to install and import math, and used a small dataset with a random distribution, I used the build-in "sum" and "length" functions and used the math module to get the square root of the variance to get the calculations.

---
+++ ImportedMathModule.py
@@ -0,0 +1,12 @@
+import math
+
+data =[12,18,14,20,16]
+
+mean = sum(data) / len(data)
+
+variance = sum((x-mean)**2 for x in data) / len(data)
+std_deviation = math.sqrt(variance)
+
+print(f"Mean: {mean}")
+print(f"Variance: {variance}")
+print(f"Standard Deviation:{std_deviation}")
